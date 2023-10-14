64b9ccfba601a47203223aeedeaa72d46e185be9
Anthony Maisincho <thonymmj21@gmail.com>
2023-10-14 07:56:33 -0500

    Se completo hasta el ejercicio 7

--- Ejercicio2/Core/Inc/asm_func.h
+++ Ejercicio2/Core/Inc/asm_func.h
@@ -11,3 +11,27 @@ void asm_zeros(uint32_t * vectorIn, uint32_t longitud);
 
 //Ejercicio 2
 void asm_productoescalar32(uint32_t * vectorIn, uint32_t * vectorOut ,uint32_t longitud, uint32_t escalar);
+
+//Ejercicio 3
+void asm_productoEscalar16 (uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitud, uint16_t escalar);
+
+//Ejercicio 4
+
+void asm_productoEscalar12 (uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitud, uint16_t escalar);
+
+
+//Ejercicio 5
+
+
+//Ejercicio 6
+void asm_pack32to16(int32_t * vectorIn, int16_t *vectorOut, uint32_t longitud);
+
+//Promedio en ASM normal
+
+void asm_prom(uint16_t *vectorOut,uint16_t *vectorX,uint16_t *vectorY,uint16_t longitud);
+
+//Promedio en ASM con SIMD
+
+void asm_promSIMD(uint16_t *vectorOut,uint16_t *vectorX,uint16_t *vectorY,uint16_t longitud);
+
+
